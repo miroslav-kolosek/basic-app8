9c8cdeb767f543aede1655847ac5c1cccec0a8ae
miroslav <miroslav@kolosek.com>
2023-06-27 10:45:00 +0200

    reek code smell example

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
       "Item name is empty."
     end
   end
+
+  def full_message
+    x = 10
+    a = nil
+    if name.present?
+        "Full message: Item name is #{name} " + "  and id is " +  id
+    else
+      "Full Message:   Item name is empty. "
+    end
+  end
 end
